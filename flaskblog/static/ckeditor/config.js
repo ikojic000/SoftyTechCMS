87a8cbac7842003461d4833aa9ed3b68666e28d5
Ivan Kojic <35032112+ikojic000@users.noreply.github.com>
2019-03-01 10:09:15 +0100

    Gallery *NEEDS FIXING*

--- flaskblog/static/ckeditor/config.js
+++ flaskblog/static/ckeditor/config.js
@@ -8,4 +8,6 @@ CKEDITOR.editorConfig = function( config ) {
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 	config.extraPlugins = 'youtube';
+	config.extraPlugins = 'filebrowser';
+	//config.extraPlugins = 'slideshow';
 };
