1150e6c7acc7bb7d123eaf5bf56be6fb0cf2a92a
Ivan Kojic <35032112+ikojic000@users.noreply.github.com>
2019-09-25 20:05:37 +0200

    MySQL Database - radi EMAIL  - ne radi

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -4,9 +4,10 @@ import os
 
 filemanager_route = os.path.join(app.root_path, 'static/upload')
 
+
 class Config:
     SECRET_KEY = 'y.o.u.s.h.a.l.l.n.o.t.k.n.o.w'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:administrator@localhost/softy?charset=utf8mb4'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
@@ -23,3 +24,10 @@ class Config:
     USER_ENABLE_USERNAME = False
     USER_AUTO_LOGIN_AFTER_RESET_PASSWORD = False
     FLASKFILEMANAGER_FILE_PATH = filemanager_route
+
+# ALTER TABLE comment CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+# ALTER TABLE comment modify name text charset utf8mb4;
+
+# 'mysql://root:administrator@localhost/softy'
+# 'mysql+pymysql://root:administrator@localhost/softy'
+# 'sqlite:///site.db'
